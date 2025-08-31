9a1936ed4284af4a7f8a9963386f20c056d4a120
mohammad-sameer-shishani <mohammad.s.shishani@gmail.com>
2025-08-31 23:16:13 +0300

    feat(client): implement error handling with custom error pages and middleware End of Section 7

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { Home } from '../features/home/home';
 import { Lists } from '../fearures/lists/lists';
 import { Messages } from '../fearures/messages/messages';
 import { authGuard } from '../core/guards/auth-guard';
+import { TestErrors } from '../features/test-errors/test-errors';
+import { NotFound } from '../shared/errors/not-found/not-found';
+import { ServerError } from '../shared/errors/server-error/server-error';
 
 export const routes: Routes = [
     {path: '', component: Home},
@@ -15,6 +18,9 @@ export const routes: Routes = [
                 {path: 'members', component: MemberList,canActivate: [authGuard]},
                 {path: 'members/:id', component: MemberDetailed},
                 {path: 'lists', component: Lists},
-                {path: 'messages', component: Messages},]},
-    {path: '**', component: Home},
+                {path: 'messages', component: Messages},
+            ]},
+            {path: 'errors', component: TestErrors},
+            {path: 'server-error', component: ServerError},
+            {path: '**', component: NotFound},
 ];
